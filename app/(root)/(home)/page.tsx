6ef16ee8601d4b055dc5330c8153ccc2e7d7f188
Zeenat <zeenatkm301@gmail.com>
2024-07-15 19:47:26 +0530

    cler auth added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,32 +1,12 @@
-// //The layout in the home route group will be responsible for the layout of all the pages in (home)
-// import React from "react";
+//The layout in the home route group will be responsible for the layout of all the pages in (home)
+import React from "react";
 
-// const Home = () => {
-//   return (
-//     <section className="flex size-full flex-col gap-10 text-white">
-//       <h1 className="text-3xl font-bold">Home </h1>
-//     </section>
-//   );
-// };
-
-// export default Home;
-"use client";
-import React,{Suspense, useEffect} from "react";
-const LazyComponent=React.lazy(()=>import ('./personal-room/page'))
-
-const page = () => {
-  useEffect(() => {
-    console.log('Effect runs after every render');
-  });
-  useEffect(() => {
-    console.log('Effect runs only once after initial render');
-  }, []);
-  
+const Home = () => {
   return (
-    <Suspense fallback={<>Loading...</>}>
-      <LazyComponent/>
-    </Suspense>
-  )
-}
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Home </h1>
+    </section>
+  );
+};
 
-export default page
+export default Home;
